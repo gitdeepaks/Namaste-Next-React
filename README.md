33ec5fbfca75560764ce37fcfc75a6310b94e12d
gitdeepaks <deepaksankhyan92@outlook.com>
2023-05-26 20:56:57 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# _Namaste React_ 🚀
+
+## Parcel
+
+- Dev
+- Local Server
+- Automatic Refresh i.e. HMR = Hot Module Replacement
+- File Watching Algorithem i.e. written in C++.
+- Faster Builds because if cashing.
+- Image Optimization
+- Minification of file
+- Bundling
+- Compress the file
+- Consistant Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Daignostics
+- Error Handling
+- HTTPS Server Support
+- Tree Shaking
+- Remove Unused code for you.
+- Types of Exports in REACT :
+  - export Defaults
+  - components from "path"
+
+## _React Hooks_ (Normal JS Utility functions)
+
+- useState() - superpowerful react variables
+- useEffect()
---
+++ src/components/Body.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import RestaurantCart from "./RestaurantCart";
+import resObj from "../utils/mockData";
+
+const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resObj);
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.data.avgRating > 4.0
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCart key={restaurant.data.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
