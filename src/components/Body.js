99e3866fb175f7ef4564296700562aa3866f7aea
gitdeepaks <deepaksankhyan92@outlook.com>
2023-06-01 19:56:05 +0530

    new

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import RestaurantCart from "./RestaurantCart";
 import resObj from "../utils/mockData";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resObj);
+  const [listOfRestaurants, setListOfRestaurants] = useState(resObj); //Used to create the state variables
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+  };
   return (
     <div className="body">
       <div className="filter">
