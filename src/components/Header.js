16dc2d09e54f809009864f1a29ec3f9fb228cd53
gitdeepaks <deepaksankhyan92@outlook.com>
2023-06-02 23:31:40 +0530

    new

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,7 +19,9 @@ const Header = () => {
           <button
             className="login"
             onClick={() => {
-              setBtnNameReact("Logout");
+              btnNameReact === "Login"
+                ? setBtnNameReact("Logout")
+                : setBtnNameReact("Login");
             }}
           >
             {btnNameReact}
