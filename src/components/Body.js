033e15deedeb5da4196a85786327391a0b526e10
gitdeepaks <deepaksankhyan92@outlook.com>
2023-06-02 20:39:41 +0530

    new

--- src/components/Body.js
+++ src/components/Body.js
@@ -19,18 +19,21 @@ const Body = () => {
     setListOfRestaurants(json?.data?.cards[2]?.data?.data?.cards);
   };
 
-  if (listOfRestaurants.length === 0) {
-    return <Shimmer />;
-  }
+  //conditional Rendering
+  // if (listOfRestaurants.length === 0) {
+  //   return <Shimmer />;
+  // }
 
-  return (
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
             const filteredList = listOfRestaurants.filter(
-              (res) => res.data.avgRating > 4.0
+              (res) => res.data.avgRating > 4.1
             );
             setListOfRestaurants(filteredList);
           }}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,33 @@
-import React from 'react'
-import { LOGO_URL } from '../utils/constants';
+import React from "react";
+import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 
 const Header = () => {
-    return (
-        <div className="header">
-            <div className="logo-container">
-                <img className="logo" src={LOGO_URL} />
-            </div>
-            <div className="nav-items">
-                <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
-                    <li>Cart</li>
-                </ul>
-            </div>
-        </div>
-    );
+  //   let btnName = "Login";
+  const [btnNameReact, setBtnNameReact] = useState("Login");
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              setBtnNameReact("Logout");
+            }}
+          >
+            {btnNameReact}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
-export default Header
+export default Header;
